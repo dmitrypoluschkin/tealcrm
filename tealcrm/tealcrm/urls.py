ce1fd25412653a829aede3ff2563b54ec608068d
unknown <19polushkin.dima87@gmail.com>
2024-10-29 06:24:40 +0400

    crete about page

--- tealcrm/tealcrm/urls.py
+++ tealcrm/tealcrm/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, about
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
 ]
