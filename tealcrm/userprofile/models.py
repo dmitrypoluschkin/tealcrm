8027690cb4c73b5a05097af55abe27af7fde53f1
unknown <19polushkin.dima87@gmail.com>
2024-10-30 06:26:46 +0400

    add database userprofile

--- tealcrm/userprofile/models.py
+++ tealcrm/userprofile/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Userprofile(models.Model):
+    name = models.OneToOneField(User, related_name="userprofile", on_delete=models.CASCADE)
