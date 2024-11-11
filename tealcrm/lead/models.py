4ef3c950c6a71fcd53195f7ef019e9e89305a6ef
unknown <19polushkin.dima87@gmail.com>
2024-11-11 06:43:29 +0400

    add database models

--- tealcrm/lead/models.py
+++ tealcrm/lead/models.py
@@ -25,11 +25,15 @@ class Lead(models.Model):
         (LOST, 'Lost'),
     )
 
-    name = models.ChaeField(max_length=255)
-    email = models.EmaiField()
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
     description = models.TextField(blank=True, null=True)
-    priority = models.CharField(max_length=10, coices=CHOICES_PRIORITY, default=MEDIUM)
-    status = models.CharField(max_length=10, coices=COICES_STATUS, default=NEW)
+    priority = models.CharField(max_length=10, choices=CHOICES_PRIORITY, default=MEDIUM)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=NEW)
     created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
