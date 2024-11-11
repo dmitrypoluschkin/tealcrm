6437ca71c323249c6a51b5d6d66fbb970a470504
unknown <19polushkin.dima87@gmail.com>
2024-11-11 06:17:16 +0400

    add lead models

--- tealcrm/lead/models.py
+++ tealcrm/lead/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    LOW = 'low'
+    MEDIUM = 'medium'
+    HIGH = 'high'
+
+    CHOICES_PRIORITY = (
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    )
+
+    NEW = 'new'
+    CONTACTED = 'contacted'
+    WON = 'won'
+    LOST = 'lost'
+
+    CHOICES_STATUS = (
+        (NEW, 'New'),
+        (CONTACTED, 'Contacted'),
+        (WON, 'Won'),
+        (LOST, 'Lost'),
+    )
+
+    name = models.ChaeField(max_length=255)
+    email = models.EmaiField()
+    description = models.TextField(blank=True, null=True)
+    priority = models.CharField(max_length=10, coices=CHOICES_PRIORITY, default=MEDIUM)
+    status = models.CharField(max_length=10, coices=COICES_STATUS, default=NEW)
+    created_by = models.ForeignKey(User, related_name='leads', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
